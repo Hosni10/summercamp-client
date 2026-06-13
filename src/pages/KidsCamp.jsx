493cbee939b53c13a3b79bf1cb26081de4cf719e
Hosni10 <mohammedhosni25@gmail.com>
2026-06-13 23:59:14 +0300

    Fixing Inside Our Camp

--- src/pages/KidsCamp.jsx
+++ src/pages/KidsCamp.jsx
@@ -342,7 +342,7 @@ function KidsCamp() {
                     rel="noopener noreferrer"
                     className="inline-block bg-white text-black hover:bg-gray-100 px-6 py-2 rounded-md font-medium transition-colors"
                   >
-                    Inside our camp
+                    Inside Our Camp
                   </a>
                   <a
                     href="#location"
