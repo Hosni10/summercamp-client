b981cc5c27ac8aefa548bfee988a2d1f4ae344bf
Hosni10 <mohammedhosni25@gmail.com>
2026-07-04 14:35:09 +0300

    Adding Mubadala discount code

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -118,6 +118,7 @@ const BookingForm = ({ selectedPlan, selectedLocation, campType, onClose }) => {
     ad20nec: 20,
     Ad20noc: 20,
     "1ADNOC5": 15,
+    "1Mubadala5": 15,
     "Adnecstaff20@adss2025": 32.73,
     "20Kuwaiti": 22.08,
     "POD50@ADSS2025": 50,
